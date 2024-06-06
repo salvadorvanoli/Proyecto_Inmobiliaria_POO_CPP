88b6693c7252b2d32047db226c153f187fa6b7eb
Blckdev <23felipecamejo@gmail.com>
2024-06-06 20:27:29 -0300

    Herencias agregadas xdxd

---
+++ apartamento.h
@@ -0,0 +1,12 @@
+#ifdef APARTAMENTO_H
+#define APARTAMENTO_H
+#include <iostream>
+#include "propiedad.h"
+using namespace std;
+
+class Apartamento : public Propiedad{
+
+}
+
+
+#endif
--- propiedad.h
+++ propiedad.h
@@ -1,8 +1,47 @@
 #ifdef PROPIEDAD_H
 #define PROPIEDAD_H
+#include <iostream>
+#include "dtmensaje.h"
+#include "alquiler.h"
+#include "venta.h"
+#include "propiedad.cpp"
+using namespace std;
 
-class propiedad{
-
+class Propiedad{
+    private:
+        int codigo;
+        int cantAmbiente;
+        int cantDormitorios;
+        int cantBanios;
+        int m2Edificios;
+        int m2Totales;
+        bool tieneGaraje;
+    public:
+        propiedad(int, int, int, int, int, int, bool);
+        ~propiedad();
+        void setM2Totales(int);
+        void setM2Edificios(int);
+        void setTieneGaraje(bool);
+        void setCantBanios(int);
+        void setCantDormitorios(int);
+        void setCantAmbiente(int);
+        void setCodigo(int);
+        bool getTieneGaraje();
+        int getm2Totales();
+        int getCodigo();
+        int getCantAmbiente();
+        int getCantDormitorios();
+        int getCantBanios();
+        int getM2Edificios();
+        void asociarVenta(Venta);
+        void asociarAlquier(Alquier);
+        void desvincularZona();
+        void destruirConversacion();
+        void destruirVenta();
+        void destruirAlquiler();
+        DTChatProp getDTChatProp(string);
+        DTMensaje getUltimosmensajes();
+        Conversacion nuevoChat();
 };
 
 #endif
