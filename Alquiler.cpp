cdc03c646293e7f60567222943869ce774724aca
joaqui-jz <164902689+joaqui-jz@users.noreply.github.com>
2024-06-09 22:03:46 -0300

    Update Alquiler.cpp

---
+++ Alquiler.cpp
@@ -0,0 +1,22 @@
+#ifndef ALQUILER_H
+#define ALQUILER_H
+#include <iostream>
+#include "ICollection/interfaces/ICollectible.h"
+using namespace std;
+
+Alquiler:: Alquiler(float precioAlquiler) {
+    this->precioAlquiler = precioAlquiler;
+};
+
+Alquiler:: ~Alquiler(){
+};
+
+float Alquiler:: getprecioAlquiler(){
+    return precioAlquiler;
+};
+
+void Alquiler:: setprecioAlquiler(float precioAlquiler) {
+    this->precioAlquiler = precioAlquiler;
+};
+
+#endif
