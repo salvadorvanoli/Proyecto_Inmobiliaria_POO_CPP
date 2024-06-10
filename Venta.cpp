4ba0526393c1bdc51d1977a8c24ef2727b225afe
joaqui-jz <164902689+joaqui-jz@users.noreply.github.com>
2024-06-09 22:08:26 -0300

    Update Venta.cpp

---
+++ Venta.cpp
@@ -0,0 +1,23 @@
+#ifndef VENTA_H
+#define VENTA_H
+#include <iostream>
+#include "ICollection/interfaces/ICollectible.h"
+using namespace std;
+
+Venta:: Venta(float precioTotal) {
+    this->precioTotal= precioTotal;
+};
+
+Venta:: ~Venta() {
+};
+
+float Venta::getprecioTotal() {
+    return precioTotal;
+};
+
+void Venta:: setprecioTotal(int precioTotal) {
+    this->precioTotal = precioTotal;
+};
+
+#endif
+
